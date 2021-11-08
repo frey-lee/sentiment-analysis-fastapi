e48ee272ff4e614eb07b885f16dcf1dc4da71fa7
Frey <93508481+frey-lee@users.noreply.github.com>
2021-11-08 22:42:58 +0800

    Added flake8 and black

--- sent_fast_api/sent_api.py
+++ sent_fast_api/sent_api.py
@@ -5,14 +5,13 @@ from sent_fast_api.model import SentimentModel, SentimentQueryModel
 app = FastAPI()
 model = SentimentModel()
 
-@app.post('/predict')
+
+@app.post("/predict")
 def predict(data: SentimentQueryModel):
     data = data.dict()
-    polarity, subjectivity = model.get_sentiment(data['text'])
-    return {
-        'polarity': polarity,
-        'subjectivity': subjectivity
-    }
+    polarity, subjectivity = model.get_sentiment(data["text"])
+    return {"polarity": polarity, "subjectivity": subjectivity}
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
--- tests/test_sent_fast_api.py
+++ tests/test_sent_fast_api.py
@@ -1,15 +1,16 @@
 from sent_fast_api import __version__
 from sent_fast_api.model import SentimentModel
-import uvicorn
+
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
 
 def test_model():
     model = SentimentModel()
-    polarity, subjectivity = model.get_sentiment('I love you three thousand')
-    polarity = round(polarity,1)
-    subjectivity = round(subjectivity,1)
+    polarity, subjectivity = model.get_sentiment("I love you three thousand")
+    polarity = round(polarity, 1)
+    subjectivity = round(subjectivity, 1)
     assert polarity <= 1
     assert polarity >= -1
     assert subjectivity <= 1
